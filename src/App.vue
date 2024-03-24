d7fb738daf5fa94d2409b29b62a534b1513c9133
Andrea <andrea.dauria@fanplayr.com>
2024-03-24 01:01:39 +0100

    Add: Typing placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <p href="https://vuejs.org/" target="_blank">
+      <h1 class="typing">Hello! My name is Andrea D'Auria</h1>
+    </p>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<script>
+export default {
+  name: 'App',
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  font-family: 'Roboto', sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.app-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #333; /* Dark grey background */
+}
+
+.typing {
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: .15em solid orange; /* Cursor like styling */
+  animation: typing 4s steps(40, end), blink-caret .75s step-end infinite;
+  color: white; /* Light color text for visibility on dark background */
+}
+
+/* Typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* Cursor animations */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="app-container">
+            <h1 class="typing">Welcome to my portfolio</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
